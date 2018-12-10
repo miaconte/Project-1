f19143fa239095a26c2bcb335bf1fffc10dba76e
Conte <coding@Contes-MacBook-Pro.local>
2018-12-10 14:26:32 -0800

    working map, added LA lat + lng

--- assets/javascript/lol.js
+++ assets/javascript/lol.js
@@ -2,7 +2,7 @@
       var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
+          center: {lat: 118.2468, lng: 34.0407},
           zoom: 6
         });
         infoWindow = new google.maps.InfoWindow;
